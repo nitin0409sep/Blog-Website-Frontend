29986bdfadecc836e15b3c1b74bb0db288bacf0f
nitin0409verma <nitin.verma@remotestate.com>
2024-09-16 15:53:24 +0530

    Removed Unwanted Console

--- src/components/common/NoPostsMessage.jsx
+++ src/components/common/NoPostsMessage.jsx
@@ -5,15 +5,15 @@ const NoPostsMessage = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center">
+    <div className="flex flex-col items-center justify-center h-full text-center">
       <h1 className="font-semibold text-gray-100 mb-4 text-5xl">
-        You haven't added any posts yet 🙂
+        You haven't added any posts yet 🙁🙂
       </h1>
-      <p className="text-gray-500 mb-6 text-2xl">
+      <p className="text-gray-300 mb-6 text-2xl">
         Start sharing your thoughts with the world by adding your first post!
       </p>
       <button
-        className="bg-blue-500 hover:bg-blue-600 text-white font-semibold` py-4 px-6 rounded text-xl"
+        className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-4 px-6 rounded text-xl"
         onClick={() => {
           navigate("/addPost");
         }}
