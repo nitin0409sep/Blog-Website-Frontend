b14acc5a4d3f187e50151f0c21583ca4878e0f17
nitin0409verma <nitin.verma@remotestate.com>
2024-09-22 17:19:33 +0530

    UI for post view (particular post)

--- src/components/common/Post.jsx
+++ src/components/common/Post.jsx
@@ -10,7 +10,7 @@ import { Spinner } from "./Loader";
 
 const Post = () => {
   const { id } = useParams();
-  const { register, formState, getValues } = useForm();
+  const { register, formState, getValues, handleSubmit } = useForm();
   const { errors, isSubmitting, isValid } = formState;
 
   const {
@@ -24,6 +24,10 @@ const Post = () => {
     },
   });
 
+  function addComment() {
+    console.log(getValues());
+  }
+
   if (isFetching || isLoading) {
     return <GlobalLoader></GlobalLoader>;
   }
@@ -32,84 +36,192 @@ const Post = () => {
 
   return (
     <>
-      <div className="flex flex-col font-light text-white text-3xl font-serif">
-        {/* Image */}
-        <div>
-          <img
-            src={post.img_url}
-            loading="lazy"
-            alt="Post image"
-            style={{ height: "400px", objectFit: "cover" }}
-            onError={(e) => {
-              const target = e.target;
-              target.onerror = null; // Prevent loop if fallback fails
-              target.src = "/no-image.svg";
-            }}
-          />
-        </div>
+      {/* For Small-Medium Devices Upto 1020 px */}
+      <div className="flex flex-col h-full justify-between lg:hidden">
+        <div className="flex flex-col font-light text-white text-3xl font-serif">
+          {/* Image */}
+          <div>
+            <img
+              src={post.img_url}
+              loading="lazy"
+              alt="Post image"
+              style={{ objectFit: "cover", height: "400px" }}
+              onError={(e) => {
+                const target = e.target;
+                target.onerror = null; // Prevent loop if fallback fails
+                target.src = "/no-image.svg";
+                target.style.height = "200px"; // Change height on error
+              }}
+            />
+          </div>
 
-        {/* Title */}
-        <div className=" p-5 text-3xl text-pink-400 ">
-          Title - {post.post_name}
-        </div>
+          {/* Title */}
+          <div className=" p-5 text-3xl text-pink-400 ">
+            Title - {post.post_name}
+          </div>
 
-        {/* Description */}
-        <div className=" p-5 text-3xl ">Desc - {post.post_desc}</div>
+          {/* Description */}
+          <div className=" p-5 text-3xl ">Desc - {post.post_desc}</div>
 
-        {/* Writer & Likes */}
-        <div className="flex h-20 p-5 justify-between text-4xl">
-          <span className="text-purple-400 ">
-            Written By - {post.user_name}
-          </span>
+          <hr />
+          {/* Writer & Likes */}
+          <div className="flex h-20 p-5 justify-between text-4xl">
+            <span className="text-purple-400 ">
+              Written By - {post.user_name}
+            </span>
 
-          <div className="hover: text-green-400 flex justify-center items-center gap-1 hover:text-blue-400">
-            <ThumbUpIcon fontSize="large" />{" "}
-            <span className="text-4xl">Like - {post.likes ?? 0}</span>
+            <div className="hover: text-green-400 flex justify-center items-center gap-1 hover:text-blue-400">
+              <ThumbUpIcon fontSize="large" />{" "}
+              <span className="text-4xl">Like - {post.likes ?? 0}</span>
+            </div>
+          </div>
+          <hr />
+
+          {/* Article */}
+          <div className="flex-1 text-pretty hyphens-auto text-justify p-5 leading-relaxed">
+            <span className="text-white font-serif">{post?.post_article}</span>
           </div>
         </div>
 
-        {/* Article */}
-        <div className="flex-1 text-pretty hyphens-auto text-justify p-5">
-          <span className="text-white font-serif">{post?.post_article}</span>
+        {/* Comments */}
+        <div className="p-5 text-4xl flex flex-col  gap-4">
+          <hr />
+
+          <span className="text-white">Comments -</span>
+          <div className="flex gap-3">
+            <input
+              type="text"
+              placeholder="Please add your valuable comments..."
+              className="w-full p-4 rounded-xl border-none text-2xl outline-none"
+              {...register("comment", {
+                required: {
+                  value: true,
+                },
+              })}
+            />
+            {errors.comment && (
+              <p className="text-red-400 text-lg pl-2">{errors.desc.message}</p>
+            )}
+
+            <div className="flex align-bottom justify-center rounded-xl text-center">
+              <button
+                type="submit"
+                className={`p-4 pl-10 pr-10 rounded-lg text-2xl flex justify-center ${
+                  !isValid
+                    ? "bg-gray-500 text-white cursor-not-allowed"
+                    : "bg-green-400 text-white hover:scale-100 transform transition-all duration-300 ease-in-out"
+                }`}
+                disabled={!isValid || isSubmitting}
+              >
+                {isSubmitting ? <Spinner height={20} width={20} /> : "Add"}
+              </button>
+            </div>
+          </div>
+
+          <hr />
         </div>
       </div>
 
-      <hr />
-      {/* Comments */}
-      <div className="h-40 p-5 text-4xl flex flex-col gap-4">
-        <span className="text-white">Comments -</span>
-        <div className="flex gap-3">
-          <input
-            type="text"
-            placeholder="Add add your valuable comments..."
-            className="w-full p-4 rounded-xl border-none text-2xl outline-none"
-            {...register("comment", {
-              required: {
-                value: true,
-              },
-            })}
-          />
-          {errors.comment && (
-            <p className="text-red-400 text-lg pl-2">{errors.desc.message}</p>
-          )}
+      {/* For Large Devices Above 1020 px */}
+      <div className="hidden lg:flex sm:flex-col h-full justify-between">
+        <div
+          className="grid font-light text-white text-4xl font-serif h-full grid-cols-5 gap-x-5"
+          style={{
+            height: "80vh",
+          }}
+        >
+          {/* Content */}
+          <div className="flex flex-col col-span-3" style={{ height: "80vh" }}>
+            {/* Title */}
+            <div className="p-5 text-4xl text-pink-400">
+              Title - {post.post_name}
+            </div>
 
-          <div className="flex justify-center rounded-xl text-center">
-            <button
-              type="submit"
-              className={`p-4 rounded-lg text-2xl flex justify-center ${
-                !isValid
-                  ? "bg-gray-500 text-white cursor-not-allowed"
-                  : "bg-green-400 text-white hover:scale-100 transform transition-all duration-300 ease-in-out"
-              }`}
-              disabled={!isValid || isSubmitting}
-            >
-              {isSubmitting ? <Spinner height={20} width={20} /> : "Add"}
-            </button>
+            {/* Description */}
+            <div className="p-5 text-3xl">Desc - {post.post_desc}</div>
+
+            <hr className="border-gray-700" />
+
+            {/* Writer & Likes */}
+            <div className="flex h-24 p-5 justify-between text-3xl">
+              <span className="text-purple-400">
+                Written By - {post.user_name}
+              </span>
+
+              <div className="text-green-400 flex justify-center items-center gap-1 hover:text-blue-400 cursor-pointer">
+                <ThumbUpIcon fontSize="large" />
+                <span className="text-3xl">Like - {post.likes ?? 0}</span>
+              </div>
+            </div>
+            <hr className="border-gray-700" />
+
+            {/* Article */}
+            <div className="pt-5 pl-5 pb-5 leading-relaxed flex-1 text-pretty hyphens-auto text-justify overflow-auto">
+              <span className="text-white font-serif">
+                {post?.post_article}
+              </span>
+            </div>
+          </div>
+
+          {/* Image */}
+          <div className="col-span-2 rounded-lg mt-1 mr-2">
+            <img
+              src={post.img_url}
+              loading="lazy"
+              alt="Post image"
+              className="object-cover w-full h-full rounded-lg"
+              onError={(e) => {
+                const target = e.target;
+                target.onerror = null; // Prevent loop if fallback fails
+                target.src = "/no-image.svg";
+              }}
+            />
           </div>
         </div>
-      </div>
 
-      <hr />
+        {/* Comments */}
+        <div className="pl-5 pr-5 text-3xl flex flex-col gap-4">
+          <hr className="border-gray-700" />
+
+          <span className="text-white">Comments -</span>
+          <form
+            className="w-full flex gap-3"
+            onSubmit={handleSubmit(addComment)}
+          >
+            <input
+              type="text"
+              placeholder="Please add your valuable comments..."
+              className="w-full p-5 rounded-xl border-none text-xl outline-none"
+              {...register("comment", {
+                required: {
+                  value: true,
+                },
+              })}
+            />
+            {errors.comment && (
+              <p className="text-red-400 text-lg pl-2">
+                {errors.comment.message}
+              </p>
+            )}
+
+            <div className="flex items-center justify-center rounded-xl text-center">
+              <button
+                type="submit"
+                className={`pt-5 pb-5 pl-32 pr-32 rounded-lg text-2xl flex justify-center ${
+                  !isValid
+                    ? "bg-gray-500 text-white cursor-not-allowed"
+                    : "bg-green-400 text-white hover:scale-105 transform transition-all duration-300 ease-in-out"
+                }`}
+                disabled={!isValid || isSubmitting}
+              >
+                {isSubmitting ? <Spinner height={20} width={20} /> : "Add"}
+              </button>
+            </div>
+          </form>
+
+          <hr className="border-gray-700" />
+        </div>
+      </div>
     </>
   );
 };
